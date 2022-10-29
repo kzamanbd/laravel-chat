1b35894f1a53fd1bf9f08070abd87e2210083b38
Kamruzzaman <kzamanbn@gmail.com>
2022-10-29 16:15:13 +0600

    Update live-message.blade.php

--- resources/views/livewire/live-message.blade.php
+++ resources/views/livewire/live-message.blade.php
@@ -134,6 +134,7 @@
                                     <div class="flex-grow ml-4">
                                         <div class="relative w-full">
                                             <input type="text" wire:model="newMessage"
+                                                wire:keydown.enter="sendMessage()"
                                                 class="flex w-full border rounded-xl focus:outline-none focus:border-indigo-300 pl-4 h-10" />
                                             <button
                                                 class="absolute flex items-center justify-center h-full w-12 right-0 top-0 text-gray-400 hover:text-gray-600">
@@ -183,6 +184,7 @@
                                     <div class="flex-grow ml-4">
                                         <div class="relative w-full">
                                             <input type="text" wire:model="newMessage"
+                                                wire:keydown.enter="sendMessage()"
                                                 class="flex w-full border rounded-xl focus:outline-none focus:border-indigo-300 pl-4 h-10" />
                                             <button
                                                 class="absolute flex items-center justify-center h-full w-12 right-0 top-0 text-gray-400 hover:text-gray-600">
