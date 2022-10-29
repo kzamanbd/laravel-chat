febe35c9bdf6dc95b057fee5ed3557916277d1cf
Kamruzzaman <kzamanbn@gmail.com>
2022-10-29 12:59:02 +0600

    added conversation relationship on message model

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     use HasFactory;
+
+    protected $appends = ["send_at"];
+    public function getSendAtAttribute()
+    {
+        return $this->created_at->diffForHumans();
+    }
+
+    public function conversation()
+    {
+        return $this->belongsTo(Conversation::class, "conversation_id", "id");
+    }
 }
