a310ddcba85118167bbcc5ae3d2381779bb6498e
Kamruzzaman <kzamanbn@gmail.com>
2022-11-07 09:21:10 +0600

    bug fixing and improve performance

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,6 +3,9 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
         </h2>
+        <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
+            Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+        </div>
     </x-slot>
 
     <div class="py-12">
