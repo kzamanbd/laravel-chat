06670b84a07c80a0b1d9dbeb79213d769947f0de
Kamruzzaman <kzamanbn@gmail.com>
2022-12-25 22:10:48 +0600

    update js and css files

--- resources/views/layouts/b-layout.blade.php
+++ resources/views/layouts/b-layout.blade.php
@@ -9,10 +9,10 @@
     <!-- App favicon -->
     <link rel="shortcut icon" href="{{ asset('chat-ui/images/favicon.ico') }}" />
     <!-- magnific-popup css -->
-    <link href="{{ asset('chat-ui/libs/magnific-popup/magnific-popup.css') }}" rel="stylesheet" />
+    <link href="{{ asset('chat-ui/plugins/magnific-popup/magnific-popup.css') }}" rel="stylesheet" />
     <!-- owl.carousel css -->
-    <link rel="stylesheet" href="{{ asset('chat-ui/libs/owl.carousel/assets/owl.carousel.min.css') }}" />
-    <link rel="stylesheet" href="{{ asset('chat-ui/libs/owl.carousel/assets/owl.theme.default.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('chat-ui/plugins/owl.carousel/assets/owl.carousel.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('chat-ui/plugins/owl.carousel/assets/owl.theme.default.min.css') }}" />
     <!-- Bootstrap Css -->
     <link href="{{ asset('chat-ui/css/bootstrap.min.css') }}" rel="stylesheet" />
     <!-- Icons Css -->
@@ -32,16 +32,14 @@
     </div>
     <!-- JAVASCRIPT -->
     @livewireScripts
-    <script src="{{ asset('chat-ui/libs/jquery/jquery.min.js') }}"></script>
-    <script src="{{ asset('chat-ui/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('chat-ui/libs/simplebar/simplebar.min.js') }}"></script>
-    <script src="{{ asset('chat-ui/libs/node-waves/waves.min.js') }}"></script>
+    <script src="{{ asset('chat-ui/plugins/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('chat-ui/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('chat-ui/plugins/simplebar/simplebar.min.js') }}"></script>
+    <script src="{{ asset('chat-ui/plugins/node-waves/waves.min.js') }}"></script>
     <!-- Magnific Popup-->
-    <script src="{{ asset('chat-ui/libs/magnific-popup/jquery.magnific-popup.min.js') }}"></script>
+    <script src="{{ asset('chat-ui/plugins/magnific-popup/jquery.magnific-popup.min.js') }}"></script>
     <!-- owl.carousel js -->
-    <script src="{{ asset('chat-ui/libs/owl.carousel/owl.carousel.min.js') }}"></script>
-    <!-- page init -->
-    <script src="{{ asset('chat-ui/js/pages/index.init.js') }}"></script>
+    <script src="{{ asset('chat-ui/plugins/owl.carousel/owl.carousel.min.js') }}"></script>
     <script src="{{ asset('chat-ui/js/app.js') }}"></script>
 
     @if (isset($footer))
