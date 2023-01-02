045eadab0b8ce4498af40ac6255a2126786951b8
Kamruzzaman <kzamanbn@gmail.com>
2023-01-03 00:14:35 +0600

    Update Constrains.php

--- app/Helpers/Constrains.php
+++ app/Helpers/Constrains.php
@@ -7,7 +7,7 @@ class Constrains
     const LINK_REGEX = '/(https?:\/\/[^\s]+)/';
     const LINK_REPLACE = '<a href="$1" target="_blank">$1</a>';
     const EMAIL_REGEX = '/[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}/';
-    const EMAIL_REPLACE = '<a href="mailto:$0">$0</a>';
+    const EMAIL_REPLACE = '<a href="mailto:$0" target="_blank">$0</a>';
     const PHONE_REGEX = '/\+?(88)?0?1[3456789][0-9]{8}\b/';
-    const PHONE_REPLACE = '<a href="tel:$0">$0</a>';
+    const PHONE_REPLACE = '<a href="tel:$0" target="_blank">$0</a>';
 }
