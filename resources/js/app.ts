a1c580742047faddaf9ea88c2c15334224203d41
KAMRUZZAMAN <kzamanbn@gmail.com>
2024-03-28 04:11:43 +0600

    added global plugins

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -6,12 +6,9 @@ import { createApp, h, DefineComponent } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import plugins from './Plugins';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
-
-// components
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import Simplebar from 'simplebar-vue';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
@@ -21,12 +18,11 @@ createInertiaApp({
             import.meta.glob<DefineComponent>('./Pages/**/*.vue')
         ),
     setup({ el, App, props, plugin }) {
-        const app = createApp({ render: () => h(App, props) })
+        createApp({ render: () => h(App, props) })
             .use(plugin)
-            .use(ZiggyVue);
-        app.component('AuthenticatedLayout', AuthenticatedLayout);
-        app.component('Simplebar', Simplebar);
-        app.mount(el);
+            .use(ZiggyVue)
+            .use(plugins)
+            .mount(el);
     },
     progress: {
         color: '#4B5563'
