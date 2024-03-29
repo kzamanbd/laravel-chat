31e223c935aac45c6e6e2aa059cbc8f02859ef45
KAMRUZZAMAN <kzamanbn@gmail.com>
2024-03-29 20:40:39 +0600

    added uuid

--- database/migrations/2024_03_23_202428_create_conversations_table.php
+++ database/migrations/2024_03_23_202428_create_conversations_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('conversations', function (Blueprint $table) {
             $table->id();
+            $table->uuid('uuid')->unique();
             $table->foreignId('from_user_id')
                 ->constrained('users')
                 ->onDelete('cascade');
