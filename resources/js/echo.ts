bcf66183bfa52245169809dd8d2d3c4ca5398e47
KAMRUZZAMAN <kzamanbn@gmail.com>
2024-04-15 11:07:35 +0600

    update broadcasting driver

--- resources/js/echo.ts
+++ resources/js/echo.ts
@@ -7,8 +7,8 @@ import Pusher from 'pusher-js';
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
     wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+    wsPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
+    wssPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
     enabledTransports: ['ws', 'wss'],
 });
